374d12f06714d9399177935e03b97bd0aa72e1aa
Matt Hall <mhallrp@gmail.com>
2025-03-19 17:12:06 -0600

    case

--- src/components/navBar/navMenu/navMenu.tsx
+++ src/components/navBar/navMenu/navMenu.tsx
@@ -1,27 +1,21 @@
 import React, { forwardRef } from 'react';
 
-interface NavMenuProps {
-  // scrollTo: (sectionID: string) => void;
-}
-
-const NavMenu = forwardRef<HTMLDivElement, NavMenuProps>(({ }, ref) => {
+const NavMenu = forwardRef<HTMLDivElement>((_, ref) => {
     return (
       <div ref={ref} className="bg-white w-full">
-          <button className="w-full text-sm py-2 text-center text-sm font-bold">
+          <button className="w-full text-sm py-2 text-center font-bold">
             <span className="cursor-pointer">How it Works</span>
           </button>
-          <button className="w-full text-sm py-2 text-center text-sm font-bold">
+          <button className="w-full text-sm py-2 text-center font-bold">
           <span className="cursor-pointer">Who it&apos;s for</span>
           </button>
-          <button className="w-full text-sm py-2 text-center text-sm font-bold">
+          <button className="w-full text-sm py-2 text-center font-bold">
           <span className="cursor-pointer">Blog</span>
           </button>
       </div>
     );
-  });
+});
 
 NavMenu.displayName = 'NavMenu';
-  
+
 export default NavMenu;
-
-
