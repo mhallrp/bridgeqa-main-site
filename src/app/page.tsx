7da42bd019f4f9c7a0b970b2dae363a906e5398f
Matt Hall <mhallrp@gmail.com>
2025-03-19 16:50:31 -0600

    case

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/landing/header/Index";
-import LandingBannerSection from "@/components/landing/landingBannerSection";
-import LandingSectionOne from "@/components/landing/landingSectionOne/Index";
-import LandingSectionThree from "@/components/landing/landingSectionThree/Index";
-import LandingSectionTwo from "@/components/landing/landingSectionTwo";
+// import LandingBannerSection from "@/components/landing/landingBannerSection";
+// import LandingSectionOne from "@/components/landing/landingSectionOne/Index";
+// import LandingSectionThree from "@/components/landing/landingSectionThree/Index";
+// import LandingSectionTwo from "@/components/landing/landingSectionTwo";
 import NavBar from "@/components/navBar/Index";
 
 export default function Home() {
@@ -13,7 +13,7 @@ export default function Home() {
       <section className="max-w-7xl pt-20 px-8 mx-auto" id="top">
         <Header />
       </section>
-      <section className="max-w-7xl px-8 py-24 mx-auto" id="sectionone">
+      {/* <section className="max-w-7xl px-8 py-24 mx-auto" id="sectionone">
         <LandingSectionOne />
       </section>
       <section className="max-w-7xl px-8 py-24 mx-auto" id="sectiontwo">
@@ -24,7 +24,7 @@ export default function Home() {
       </section>
       <section className="max-w-7xl px-8 py-24 mx-auto" id="sectionthree">
         <LandingSectionThree />
-      </section>
+      </section> */}
       <section id="sectionthree">
         <Footer />
       </section>
