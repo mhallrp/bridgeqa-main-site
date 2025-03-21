eb185d80213570df0075c828794d977d90551764
Matt Hall <mhallrp@gmail.com>
2025-03-21 16:26:31 -0600

    contact us

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,9 +2,9 @@ import nodemailer from 'nodemailer';
 
 export async function POST(req) {
   try {
-    const { name, email, role, teamSize, message } = await req.json();
+    const { name, email, message } = await req.json();
 
-    const transporter = nodemailer.createTransport({
+    let transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.EMAIL,
@@ -12,28 +12,18 @@ export async function POST(req) {
       },
     });
 
-    const emailBody = `
-New Early Access Request:
-
-Name: ${name}
-Email: ${email}
-Role: ${role}
-Team Size: ${teamSize}
-Message: ${message || "(No message provided)"}
-    `.trim();
-
     await transporter.sendMail({
       from: email,
       to: "info@bridgeqa.com",
-      subject: `BridgeQA Early Access Request from ${email}`,
-      text: emailBody,
+      subject: `Message from ${name}`,
+      text: "some test message",
     });
 
     return new Response(JSON.stringify({ message: 'Email sent successfully!' }), {
       status: 200,
     });
   } catch (error) {
-    console.error('❌ Error sending email:', error);
+    console.error('Error sending email:', error);
     return new Response(JSON.stringify({ message: 'Failed to send email.' }), {
       status: 500,
     });
