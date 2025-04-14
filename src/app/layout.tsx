0d2e6f96bf5a1e11fee9f804b3b5e76fdc38d8e9
Matt Hall <mhallrp@gmail.com>
2025-04-14 11:31:30 +0100

    cookies added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,36 +36,56 @@ export default function RootLayout({
   return (
 
     <html lang="en">
-<head>
-  <Script
-    id="cookieyes"
-    src="https://cdn-cookieyes.com/client_data/006bb7c873f4615cc04aecd6/script.js"
-    strategy="beforeInteractive"
-  />
+      <head>
+        <Script
+          id="cookieyes"
+          src="https://cdn-cookieyes.com/client_data/006bb7c873f4615cc04aecd6/script.js"
+          strategy="beforeInteractive"
+        />
 
-  <Script
-    async
-    src="https://www.googletagmanager.com/gtag/js?id=G-KQQNCERCDH"
-    strategy="afterInteractive"
-  />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KQQNCERCDH"
+          strategy="afterInteractive"
+        />
 
-  <Script
-    id="google-analytics"
-    strategy="afterInteractive"
-    dangerouslySetInnerHTML={{
-      __html: `
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
         window.dataLayer = window.dataLayer || [];
         function gtag(){ dataLayer.push(arguments); }
         gtag('js', new Date());
         gtag('config', 'G-KQQNCERCDH');
       `,
-    }}
-  />
-</head>
+          }}
+        />
+
+        <Script
+          id="gtm-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-WS9HZN3');
+    `,
+          }}
+        />
+
+      </head>
 
       <body
         className={`${inter.variable} ${montserrat.variable} antialiased`}
       >
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WS9HZN3P" height="0" width="0" style={{ display: "none", visibility: "hidden" }}>
+          </iframe>
+        </noscript>
+
         {/* <Amplitude /> */}
         {children}
       </body>
